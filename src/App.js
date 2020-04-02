28b1364a06a5b456f345d7132f9171c3eefed476
Tomasz Kłusak <klusaktomasz@gmail.com>
2020-04-02 21:49:36 +0200

    Add global styles for app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+// Styles
+import { createGlobalStyle } from 'styled-components';
+import 'normalize.css';
+import colors from './utils/colors';
+
+// Routes
 import Home from './views/Home';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: ${colors.whiteSoft};
+    color: ${colors.blackSoft};
+    font-family: 'Roboto', sans-serif;
+  }
+  
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+`;
 
 const App = () => (
   <BrowserRouter>
+    <GlobalStyle />
     <Switch>
       <Route path="/" component={Home} />
     </Switch>
---
+++ src/utils/colors.js
@@ -0,0 +1,8 @@
+export default {
+  white: '#fff',
+  whiteSoft: '#f1f3f5',
+
+  black: '#000',
+  blackSoft: '#272525',
+  gray: '#6c7589'
+};
