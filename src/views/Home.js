f1ce77a050d010550c0ab84a8ce9eeffd14ca7c6
Tomasz Kłusak <klusaktomasz@gmail.com>
2020-04-02 21:26:36 +0200

    Initialize project using my boilerplate

---
+++ src/views/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-family: sans-serif;
+  font-weight: 400;
+  font-size: 15px;
+`;
+
+const Home = () => (
+  <div>
+    <H1>React application</H1>
+    <img src="logo192.png" alt="" />
+  </div>
+);
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (_env, argv) => {
+  const isDev = argv.mode !== 'production';
+
+  return {
+    devtool: isDev && 'cheap-module-source-map',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/[name].[contenthash].js',
+      publicPath: '/',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: isDev ? 'development' : 'production',
+            },
+          },
+        },
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          use: 'eslint-loader',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+      }),
+      new StylelintPlugin({
+        files: '**/*.js',
+      }),
+      new CopyPlugin([
+        {
+          from: 'public',
+          to: '.',
+          ignore: ['index.html'],
+        },
+      ]),
+    ],
+    optimization: {
+      minimize: !isDev,
+      minimizer: [
+        new TerserWebpackPlugin({
+          terserOptions: {
+            compress: { comparisons: false },
+            mangle: { safari10: true },
+            output: {
+              comments: false,
+              ascii_only: true,
+            },
+            warnings: false,
+          },
+        }),
+      ],
+      splitChunks: {
+        chunks: 'all',
+        minSize: 0,
+        maxInitialRequests: 20,
+        maxAsyncRequests: 20,
+        cacheGroups: {
+          common: {
+            minChunks: 2,
+            priority: -10,
+          },
+        },
+      },
+      runtimeChunk: 'single',
+    },
+    devServer: {
+      compress: true,
+      historyApiFallback: true,
+      overlay: true,
+      stats: 'errors-only',
+    },
+  };
+};
